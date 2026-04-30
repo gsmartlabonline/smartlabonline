076c04e1e13d41bfb40dbcd3a7dd84881cfac322
gosel-coder <gosel@goselwellness.com>
2026-04-30 09:57:14 +0530

    Error message

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected 🚀"))
@@ -54,4 +57,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
-});
+});r
